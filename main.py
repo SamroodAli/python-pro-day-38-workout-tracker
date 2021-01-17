caebdccc579b66445b94290ea1676393c66e47a7
SamroodAli <samroodali@live.com>
2021-01-17 15:30:48 +0530

    API:Updating google sheets using sheety api

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import os
 import requests
-
+import datetime as dt
 # ---------------------------- CONSTANTS  ------------------------------- #
 app_id = os.environ.get("APP_ID")
 api_key = os.environ.get("API_KEY")
+
+AGE = "24"
+WEIGHT = 75
+HEIGHT = 175
+GENDER = "male"
 
 # ---------------------------- NUTRITIONIX API ENDPOINT and Headers ------------------------------- #
 api_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
@@ -20,12 +25,34 @@ user_input = input("Tell me which excercises you did: ")
 
 api_parameters = {
     "query": user_input,
-    "gender": "male",
-    "weight_kg": 75,
-    "height_cm": 175,
-    "age": 24
+    "gender": GENDER,
+    "weight_kg": WEIGHT,
+    "height_cm": HEIGHT,
+    "age": AGE
 }
 
 response = requests.post(url=api_endpoint, json=api_parameters, headers=api_headers)
 response.raise_for_status()
 exercise_data = response.json()
+
+
+# ---------------------------- Google Sheets data ------------------------------- #
+sheety_endpoint = os.environ.get("SHEETY_ENDPOINT")
+sheety_user_name = os.environ.get("SHEETY_USERNAME")
+sheety_password = os.environ.get("SHEETY_PASSWORD")
+
+date = dt.datetime.today().strftime("%d/%m/%Y")
+time = dt.datetime.now().strftime("%X")
+
+for exercise in exercise_data["exercises"]:
+    new_row_data = {
+        "workout": {
+            "date": date,
+            "time": time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+    add_row_response = requests.post(url=sheety_endpoint, json=new_row_data, auth=(sheety_user_name, sheety_password))
+    add_row_response.raise_for_status()
